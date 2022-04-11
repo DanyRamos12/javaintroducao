6358645ebf6d31caaffb9738f07cbf23712668eb
DanyRamos12 <99513107+DanyRamos12@users.noreply.github.com>
2022-04-11 13:23:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args){
+        System.out.println("LABORATÓRIO DE PROGRAMAÇÃO 2-LP2");
+        System.out.println("2 ano de informática");
+        System.out.println("seja bem vindo!");
+        System.out.println("vamos aprender java");
+  }
+}
